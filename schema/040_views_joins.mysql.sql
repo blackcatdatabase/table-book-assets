c141abc98cb0da302bd75524bb1ebd922eb679a8
Vito416 <vito416@protonmail.com>
2025-12-02 19:25:46 +0100

    Sync submodule state after YAML views + upsert/coverage fixes

--- schema/040_views_joins.mysql.sql
+++ schema/040_views_joins.mysql.sql
@@ -1,7 +1,7 @@
--- Auto-generated from joins-mysql.psd1 (map@mtime:2025-11-27T17:49:37Z)
+-- Auto-generated from joins-mysql.yaml (map@94ebe6c)
 -- engine: mysql
 -- view:   book_assets_encryption_coverage
--- Encryption coverage per asset_type
+
 CREATE OR REPLACE ALGORITHM=TEMPTABLE SQL SECURITY INVOKER VIEW vw_book_assets_encryption_coverage AS
 SELECT
   asset_type,
